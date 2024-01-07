2a2510c1ccc39b062e0848abef2d7acde4023c67
shubham4132 <shubhamg3122134@gmail.com>
2024-01-07 23:49:23 +0530

    datecounter1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
+import { useState } from "react";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(0);
+  const date = new Date("13 july 2027");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        step:{step}
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - 1)}>-</button>
+        count:{count}
+        <button onClick={() => setCount((c) => c + 1)}>+</button>
+      </div>
+      <div>
+        <span>{`${count} days from today is ${date.toDateString()}`}</span>
+      </div>
+    </>
+  );
 }
