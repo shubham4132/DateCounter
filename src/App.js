af10b79057cbbdc0012d822e452eb81bb17ccbb7
shubham4132 <shubhamg3122134@gmail.com>
2024-01-08 00:51:05 +0530

    status

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
 }
 function Counter() {
   const [count, setCount] = useState(0);
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(1);
   const date = new Date("13 july 2027");
   date.setDate(date.getDate() + count);
 
@@ -21,12 +21,19 @@ function Counter() {
         <button onClick={() => setStep((s) => s + 1)}>+</button>
       </div>
       <div>
-        <button onClick={() => setCount((c) => c - 1)}>-</button>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
         count:{count}
-        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <div>
-        <span>{`${count} days from today is ${date.toDateString()}`}</span>
+        <span>
+          {count === 0
+            ? "today is "
+            : count > 0
+            ? `${count} day from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
       </div>
     </>
   );
